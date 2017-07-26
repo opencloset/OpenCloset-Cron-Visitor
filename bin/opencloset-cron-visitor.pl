71c6d228276e07c15b464419fc38b27a9174f794
Hyungsuk Hong <aanoaa@gmail.com>
2017-07-26 19:08:47 +0900

    Add 취업날개 'extend btn' data to daily stat

--- bin/opencloset-cron-visitor.pl
+++ bin/opencloset-cron-visitor.pl
@@ -142,6 +142,47 @@ my $worker2 = do {
             my $cron = $w->cron;
             AE::log( info => "$name\[$cron] launched" );
             _collect_event_stat_daily('seoul-2017');
+
+            my $today = DateTime->today( time_zone => $TIMEZONE );
+            my $date = $today->clone->subtract( days => 1 );
+
+            for my $key (qw/offline online/) {
+                my $online = $key eq 'online' ? 1 : 0;
+                my $daily = $DB->resultset('Visitor')->search(
+                    {
+                        date   => "$date",
+                        event  => 'seoul-2017',
+                        online => $online,
+                    },
+                    { rows => 1 }
+                )->single;
+
+                next unless $daily;
+
+                my $extra = $DB->resultset('Visitor')->search(
+                    {
+                        date   => "$date",
+                        online => $online,
+                        event  => 'extra-seoul-2017'
+                    },
+                    { rows => 1 }
+                )->single;
+
+                next unless $extra;
+
+                my %columns = $daily->get_columns;
+                my %extra   = $extra->get_columns;
+                $daily->update(
+                    {
+                        visited        => $columns{visited} + $extra{visited},
+                        visited_male   => $columns{visited_male} + $extra{visited_male},
+                        visited_female => $columns{visited_female} + $extra{visited_female},
+                        visited_age_10 => $columns{visited_age_10} + $extra{visited_age_10},
+                        visited_age_20 => $columns{visited_age_20} + $extra{visited_age_20},
+                        visited_age_30 => $columns{visited_age_30} + $extra{visited_age_30},
+                    }
+                );
+            }
         }
     );
 };
